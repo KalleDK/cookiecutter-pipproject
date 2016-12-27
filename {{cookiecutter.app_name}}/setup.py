b6ec21119a27f0b8db81a460fa23a85ab79f1960
Kalle Møller <github.com@k-moeller.dk>
2016-12-27 15:48:24 +0100

    Update setup.py

--- {{cookiecutter.app_name}}/setup.py
+++ {{cookiecutter.app_name}}/setup.py
@@ -15,8 +15,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+install_requires = [x.strip() for x in all_reqs]
 
 setup(
     name='{{cookiecutter.app_name}}',
@@ -36,6 +35,5 @@ setup(
     include_package_data=True,
     author='{{cookiecutter.full_name}}',
     install_requires=install_requires,
-    dependency_links=dependency_links,
     author_email='{{cookiecutter.email}}'
 )
